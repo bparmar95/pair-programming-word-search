ad5f33619e386b5e82d40472a0fc10fc001dbac9
Baltej Parmar <bparmar@uwaterloo.ca>
2020-07-30 02:04:03 +0000

    word search code with pair

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,39 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function(matrix) {
+    let line = [];
+    let newMatrix = [];
+    let length = 0;
+    if (!(Array.isArray(matrix[0]))) {
+      return matrix
+    } else { 
+      length = matrix[0].length;
     }
-}
-
-module.exports = wordSearch
+  let col = 0;
+  while (col < length) {
+    for (let row of matrix) {
+      line.push(row[col]);
+     }
+     newMatrix.push(line);
+     line = [];
+     col += 1;
+    }
+    return newMatrix;
+  }
+  
+  const wordSearch = (letters, word) => { 
+      const horizontalJoin = letters.map(ls => ls.join(''))
+      for (l of horizontalJoin) {
+          if (l.includes(word)) {
+              return true
+          }
+      }
+  let newMatrix = transpose(letters);
+  const verticalJoin = newMatrix.map(vs => vs.join(''))
+      for (v of verticalJoin) {
+          if (v.includes(word)) {
+              return true
+          }
+      }
+      return false;
+    }
+  
+  module.exports = wordSearch
